a9bac5262d2a0ee4af4035f50416e9dd2e929b8a
t.ashula <dev@ashula.info>
2016-08-14 18:14:54 +0900

    add go gom strategy

--- lib/tachikoma/application.rb
+++ lib/tachikoma/application.rb
@@ -206,6 +206,21 @@ module Tachikoma
       end
     end
 
+    def gom
+      Dir.chdir("#{Tachikoma.repos_path}/#{@build_for}") do
+        sh(*['git', 'config', 'user.name', @commiter_name])
+        sh(*['git', 'config', 'user.email', @commiter_email])
+        sh(*['git', 'checkout', '-b', "tachikoma/update-#{@readable_time}", @base_remote_branch])
+        sh(*%w(gom install))
+        sh(*%w(gom update))
+        sh(*['git', 'add', 'Gomfile.lock'])
+        sh(*['git', 'commit', '-m', "Gom update #{@readable_time}"]) do
+          # ignore exitstatus
+        end
+        sh(*['git', 'push', @authorized_compare_url, "tachikoma/update-#{@readable_time}"])
+      end
+    end
+
     def pull_request
       @client = Octokit::Client.new access_token: @github_token
       @client.create_pull_request(
